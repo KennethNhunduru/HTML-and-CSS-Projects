7841f6566a2bcc0d4b424062de4484463c216913
KennethNhunduru <knhunduru.hotmail.com>
2023-08-30 09:37:22 +0100

    Added the Basic_HTML_Javascript_1 folder

---
+++ Basic_HTML_and_Javascript_1/Basic_Javascript_2.js
@@ -0,0 +1,7 @@
+
+	function My_First_Function() {
+		var String = "Kiss me I'm Irish!";
+        var result = String.fontcolor("Green");
+		document.getElementById("Irish").innerHTML = 
+		result;
+	}	
